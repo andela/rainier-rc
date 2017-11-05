64abe1251ba6ef859f7f826610f00a628c8c3806
Segun <segunolalive@gmail.com>
2017-11-05 08:17:49 +0100

    feat(static-page-management): parse markdown content of static page

--- client/templates/staticPages/staticPage.js
+++ client/templates/staticPages/staticPage.js
@@ -1,5 +1,6 @@
 import { StaticPages } from "/lib/collections";
 import { Template } from "meteor/templating";
+import marked from "marked";
 
 Template.staticPageDisplay.onCreated(function () {
   this.autorun(() => {
@@ -13,8 +14,9 @@ Template.staticPageDisplay.helpers({
     if (instance.subscriptionsReady()) {
       const page = StaticPages.find({slug}).fetch();
       if (page.length > 0) {
-        const data = { title, content} = page;
-        return data;
+        const data = { title, content} = page[0];
+        data.content = marked(data.content);
+        return [data];
       }
     }
   }
