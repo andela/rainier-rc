b38b912ae4ec1b3df4a67b9853287bba7ec4644d
Segun <segunolalive@gmail.com>
2017-11-05 07:08:41 +0100

    feat(static-page-management): display static pages

---
+++ client/templates/staticPages/staticPage.js
@@ -0,0 +1,21 @@
+import { StaticPages } from "/lib/collections";
+import { Template } from "meteor/templating";
+
+Template.staticPageDisplay.onCreated(function () {
+  this.autorun(() => {
+    this.subscribe("StaticPages");
+  });
+});
+
+Template.staticPageDisplay.helpers({
+  staticPage(slug) {
+    const instance = Template.instance();
+    if (instance.subscriptionsReady()) {
+      const page = StaticPages.find({slug}).fetch();
+      if (page.length > 0) {
+        const data = { title, content} = page;
+        return data;
+      }
+    }
+  }
+});
