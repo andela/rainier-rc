31a37f309309e085ac7d75f3586ebda416d98b34
Aaron Biliyok <abiliyok@gmail.com>
2017-11-07 11:51:02 +0100

    refactor(change):
     remove dead code as requested in PR

--- imports/plugins/included/product-variant/client/index.js
+++ imports/plugins/included/product-variant/client/index.js
@@ -16,8 +16,6 @@ import "./templates/products/productDetail/productImageGallery.html";
 import "./templates/products/productDetail/productImageGallery.js";
 import "./templates/products/productDetail/social.html";
 import "./templates/products/productDetail/social.js";
-// import "./templates/products/productDetail/reviews.html";
-// import "./templates/products/productDetail/reviews.js";
 
 import "./templates/products/productGrid/content.html";
 import "./templates/products/productGrid/content.js";
